5cbae9c3721bf647613b2cc51ad63c327178ed55
dac2229 <dac2229@gmail.com>
2019-08-19 00:29:45 -0700

    changed main

--- main.py
+++ main.py
@@ -35,7 +35,5 @@ def home():
 
 
 if __name__ == "__main__":
-
     port = int(os.environ.get("PORT", 6787))
-    host = os.environ.get("HOST", "127.0.0.1")
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=port)
